8210fcca84192c2c5ad00d6c3c3fd1ba03d5453f
Vinicius Bastos <106217765+viniciusbbastos@users.noreply.github.com>
2024-07-01 21:48:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
+
 function limparTexto(input) {
     if (input.value === "0" || input.value === "Custom") {
         input.value = "";
-        input.style.textAlign = "right";
-        input.style.paddingRight = "5%";
     }
 }
 
@@ -15,8 +14,6 @@ function reporTexto(input) {
 
 function reporTextoC(input) {
     if (input.value === ""){
-        input.value = "Custom";
-        input.style.textAlign = "center";
-        input.style.paddingRight = "0";
+        input.value = "Custom"
     }
 }
